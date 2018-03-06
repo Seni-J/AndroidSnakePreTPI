1424ff36d782e547c5bdc5ae17111de151c7671a
Seni-J <senistan.jegarajasingam@cpnv.ch>
2018-03-06 08:19:24 +0100

    New class (Not used for the moment)
    + Saving before using the new class

---
+++ Code/core/src/com/mygdx/game/SnakeHead.java
@@ -0,0 +1,12 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+/**
+ * Created by Senistan.JEGARAJASIN on 06.03.2018.
+ */
+
+public class SnakeHead extends SnakeTPI {
+
+
+}
--- Code/core/src/com/mygdx/game/SnakeTPI.java
+++ Code/core/src/com/mygdx/game/SnakeTPI.java
@@ -20,20 +20,27 @@ public class SnakeTPI extends ApplicationAdapter {
 	Sprite spritehead;
 	private static final float MOVE_TIME = 1F;
 	private float timer = MOVE_TIME;
-	private int moveX = 0, moveY = 0;
+	private int moveX = 520, moveY = 240;
 	private float delta = 0;
+	boolean changerotate = false;
+	boolean firsttime = true;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("snake_head.png");
 		spritehead = new Sprite(img);
+
+		spritehead.setBounds(520,240, 50,50);
 	}
 
 	@Override
 	public void render () {
 		int height = Gdx.graphics.getHeight();
 		int width = Gdx.graphics.getWidth();
+		String rotation = Float.toString(spritehead.getRotation());
+
+
 
 
 		BitmapFont font = new BitmapFont();
@@ -63,43 +70,59 @@ public class SnakeTPI extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		String istrue = Boolean.toString(touchPos.isZero());
-		spritehead.setBounds(520,240, 70,70);
+
 
 		batch.begin();
+
 		font.draw(batch, istrue, 150, 50);
-		font.draw(batch, "X:" + getSpriteX, 50, 300);
-		font.draw(batch, "Y:" + getSpriteY, 50, 250);
+		font.draw(batch, "Rotation:" + rotation, 50, 350);
+		font.draw(batch, "X:" + Gdx.input.getX(), 50, 300);
+		font.draw(batch, "Y:" + Gdx.input.getY(), 50, 250);
 		font.draw(batch, "touchpos:" + touchpos, 50, 200);
 		font.draw(batch, "angle:" + anglestr, 50, 150);
 		font.draw(batch, "height:" + heightstr, 50, 100);
 		font.draw(batch, "width:" + widthstr, 50, 50);
 
-		spritehead.draw(batch);
-		spritehead.setX(moveX);
-		spritehead.setY(moveY);
 
         timer -= delta;
 
 
 
-        if (timer <= 0 && touchPos.isZero() == true) {
+        if (timer <= 0 && Gdx.input.getX() >= 650) {
             timer = MOVE_TIME;
             if (moveX >= Gdx.graphics.getWidth()){
                 moveX = 0;
             }else {
                 moveX += 32;
-                spritehead.setRotation(-90);
+                if(changerotate == true) {
+					spritehead.rotate(-90);
+					changerotate = false;
+				}
             }
-        }else if (timer <= 0 && touchPos.isZero() == false){
-            timer = MOVE_TIME;
+        }else if (timer <= 0 && Gdx.input.getX() <= 650){
+			timer = MOVE_TIME;
+        	if (moveX >= Gdx.graphics.getWidth()){
+				moveX = 0;
+			}else {
+				moveX -= 32;
+				if(changerotate == false) {
+					spritehead.rotate90(true);
+					changerotate = true;
+				}
+			}
+
+            /*
             if (moveY >= Gdx.graphics.getHeight()) {
                 moveY = 0;
             }else {
                 moveY += 32;
                 spritehead.setRotation(0);
-            }
+            }*/
         }
 
+
+		spritehead.setPosition(moveX,moveY);
+        spritehead.draw(batch);
 
 
         //batch.draw(img, touchPos.x, height - touchPos.y);
